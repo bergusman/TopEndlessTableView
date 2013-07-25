3ef91e360bb88a5fd554ab179091318f53f5dd11
Vitaliy Berg <bergusman@gmail.com>
2013-07-25 18:12:37 +0400

    Create endless table view

--- TopEndlessTableView/VBViewController.m
+++ TopEndlessTableView/VBViewController.m
@@ -8,22 +8,80 @@
 
 #import "VBViewController.h"
 
-@interface VBViewController ()
+#define TOP_OFFSET 1000
+#define DELAY 0.3
+#define ADDING_ITEMS 50
+
+@interface VBViewController () <
+    UITableViewDataSource,
+    UITableViewDelegate
+>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@property (strong, nonatomic) NSMutableArray *items;
+@property (assign, nonatomic) BOOL loading;
 
 @end
 
 @implementation VBViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.items = [NSMutableArray array];
+    [self addItems];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)addItems {
+    for (int i = 0; i < ADDING_ITEMS; i++) {
+        NSString *item = [NSString stringWithFormat:@"%d", [self.items count]];
+        [self.items insertObject:item atIndex:0];
+    }
+}
+
+#pragma mark - UIScrollViewDelegate
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    if (scrollView.contentOffset.y < TOP_OFFSET && !self.loading) {
+        self.loading = YES;
+        
+        double delayInSeconds = DELAY;
+        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+            self.loading = NO;
+            [self addItems];
+            
+            CGPoint contentOffset = self.tableView.contentOffset;
+            CGSize oldContentSize = self.tableView.contentSize;
+            
+            [self.tableView reloadData];
+            
+            CGSize newContentSize = self.tableView.contentSize;
+            
+            contentOffset.y += newContentSize.height - oldContentSize.height;
+            self.tableView.contentOffset = contentOffset;
+        });
+    }
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.items count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellId = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
+    }
+    
+    NSString *item = self.items[indexPath.row];
+    cell.textLabel.text = item;
+    
+    return cell;
 }
 
 @end
